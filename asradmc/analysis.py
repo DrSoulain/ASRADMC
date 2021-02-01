00c5c1caee262eb7e96ed8958578bec6b0ac3d72
Anthony Soulain <anthony.soulain@gmail.com>
2021-02-01 14:35:49 +1100

    Clean analysis

--- asradmc/analysis.py
+++ asradmc/analysis.py
@@ -38,9 +38,6 @@ def load_pickle(pickle_file):
     except UnicodeDecodeError:
         with open(pickle_file, "rb") as f:
             pickle_data = pickle.load(f, encoding="latin1")
-    # except Exception as e:
-        # print("Unable to load data ", pickle_file, ":", e)
-        # raise
     return pickle_data
 
 
